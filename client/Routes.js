632668e05ff752388b4d3982bffb00bc44c20857
Marco Kwan <marcokwan10@gmail.com>
2022-01-10 18:20:28 -0500

    refactored to hooks

--- client/Routes.js
+++ client/Routes.js
@@ -1,23 +1,88 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import { Login, Signup } from './components/AuthForm';
+// import React, {Component } from 'react'
+// import {connect} from 'react-redux'
+// import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
+// import { Login, Signup } from './components/AuthForm';
+// import AuthForm from './components/AuthForm';
+// import Home from './components/Home';
+// import {me} from './store'
+
+// /**
+//  * COMPONENT
+//  */
+// class Routes extends Component {
+//   componentDidMount() {
+//     this.props.loadInitialData()
+//   }
+
+//   render() {
+//     const {isLoggedIn} = this.props
+
+//     return (
+//       <div>
+//         {isLoggedIn ? (
+//           <Switch>
+//             <Route path="/home" component={Home} />
+//             <Redirect to="/home" />
+//           </Switch>
+//         ) : (
+//           <Switch>
+//             {/* <Route path='/' exact component={ Login } /> */}
+//             <Redirect exact from='/' to='/login'/>
+//             <Route path="/login" component={AuthForm} />
+//             <Route path="/signup" component={AuthForm} />
+//           </Switch>
+//         )}
+//       </div>
+//     )
+//   }
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+//     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+//     isLoggedIn: !!state.auth.id
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     loadInitialData() {
+//       dispatch(me())
+//     }
+//   }
+// }
+
+// // The `withRouter` wrapper makes sure that updates are not blocked
+// // when the url changes
+// export default withRouter(connect(mapState, mapDispatch)(Routes))
+
+
+import React, { useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import AuthForm from './components/AuthForm';
 import Home from './components/Home';
-import {me} from './store'
+import { me } from './store';
+import { useSelector, useDispatch } from 'react-redux';
 
-/**
- * COMPONENT
- */
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData()
-  }
+const Routes = () => {
+  const { isLoggedIn } = useSelector(state => {
+    return {
+      isLoggedIn: !!state.auth.id
+    }
+  })
 
-  render() {
-    const {isLoggedIn} = this.props
+  const dispatch = useDispatch();
 
-    return (
-      <div>
+  useEffect(() => {
+    dispatch(me());
+  }, [])
+
+  return (
+    <div>
         {isLoggedIn ? (
           <Switch>
             <Route path="/home" component={Home} />
@@ -25,35 +90,14 @@ class Routes extends Component {
           </Switch>
         ) : (
           <Switch>
-            <Route path='/' exact component={ Login } />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
+            <Redirect exact from='/' to='/login'/>
+            <Route path="/login"><AuthForm formName="login" /> </Route>
+            <Route path="/signup"><AuthForm formName="signup" /></Route>
           </Switch>
         )}
       </div>
-    )
-  }
+  )
 }
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id
-  }
-}
+export default Routes
 
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
-
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import {connect} from 'react-redux'
+// import React from 'react'
+// import {connect} from 'react-redux';
 
-/**
- * COMPONENT
- */
-export const Home = props => {
-  const {username} = props
 
+// /**
+//  * COMPONENT
+//  */
+// export const Home = props => {
+//   const {username} = props
+
+//   return (
+//     <div>
+//       <h3>Welcome, {username}</h3>
+//     </div>
+//   )
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     username: state.auth.username
+//   }
+// }
+
+// export default connect(mapState)(Home)
+
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Home = () => {
+
+  const { username } = useSelector(state => state.auth);
   return (
     <div>
       <h3>Welcome, {username}</h3>
@@ -14,13 +40,5 @@ export const Home = props => {
   )
 }
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    username: state.auth.username
-  }
-}
+export default Home
 
-export default connect(mapState)(Home)
