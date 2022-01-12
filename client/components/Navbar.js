181ed9b76927b1d03bf647c0baf309587cc77127
Brandon <cohbrandon@gmail.com>
2022-01-11 19:21:48 -0500

    added tailwind framework

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,89 +1,71 @@
-// import React from 'react'
-// import {connect} from 'react-redux'
-// import {Link} from 'react-router-dom'
-// import {logout} from '../store'
-
-// const Navbar = ({handleClick, isLoggedIn}) => (
-//   <div>
-//     <h1>FS-App-Template</h1>
-//     <nav>
-//       {isLoggedIn ? (
-//         <div>
-//           {/* The navbar will show these links after you log in */}
-//           <Link to="/home">Home</Link>
-//           <a href="#" onClick={handleClick}>
-//             Logout
-//           </a>
-//         </div>
-//       ) : (
-//         <div>
-//           {/* The navbar will show these links before you log in */}
-//           <Link to="/login">Login</Link>
-//           <Link to="/signup">Sign Up</Link>
-//         </div>
-//       )}
-//     </nav>
-//     <hr />
-//   </div>
-// )
-
-// /**
-//  * CONTAINER
-//  */
-// const mapState = state => {
-//   return {
-//     isLoggedIn: !!state.auth.id
-//   }
-// }
-
-// const mapDispatch = dispatch => {
-//   return {
-//     handleClick() {
-//       dispatch(logout())
-//     }
-//   }
-// }
-
-// export default connect(mapState, mapDispatch)(Navbar)
-
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../store';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store";
 
 const Navbar = () => {
-  const { isLoggedIn } = useSelector(state => {
+  const { isLoggedIn } = useSelector((state) => {
     return {
-      isLoggedIn: !!state.auth.id
-    }
+      isLoggedIn: !!state.auth.id,
+    };
   });
 
   const dispatch = useDispatch();
 
   return (
     <div>
-      <h1>FS-App-Template</h1>
       <nav>
         {isLoggedIn ? (
           <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={() => dispatch(logout())}>
-              Logout
-            </a>
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/" onClick={() => dispatch(logout())}>
+                Sign Out
+              </Link>
+              <Link to="/">Wish List</Link>
+              <Link to="/login">Coupon</Link>
+            </div>
+            <div>
+              <Link to="/home">
+                <h1>Clothings R Us</h1>
+              </Link>
+              <Link to="#">Mens</Link>
+              <Link to="#">Womens</Link>
+              <Link to="#">Accessories</Link>
+              <Link to="#">All Clothing</Link>
+            </div>
           </div>
         ) : (
           <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/">Sign In</Link>
+              <Link to="#">Wish List</Link>
+              <Link to="/login">Coupon</Link>
+            </div>
+            <div>
+              <div>
+                <Link to="/home">
+                  <h1 className="text-3xl font-bold underline ">
+                    Clothings R Us
+                  </h1>
+                </Link>
+                <Link to="#">Mens</Link>
+                <Link to="#">Womens</Link>
+                <Link to="#">Accessories</Link>
+                <Link to="#">All Clothing</Link>
+              </div>
+              <div>
+                <button>Cart</button>
+              </div>
+            </div>
           </div>
         )}
       </nav>
       <hr />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./client/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
