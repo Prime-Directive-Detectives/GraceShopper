d32e0b6e641b2e39dd5f7191a9966036349164fd
Brandon <cohbrandon@gmail.com>
2022-01-17 22:44:05 -0500

    fixed classname in checkout span

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -207,11 +207,11 @@ const Checkout = (props) => {
           </button>
           {message && (
             <div
-              class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-2"
+              className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-2"
               role="alert"
             >
-              <span class="block sm:inline">{message}</span>
-              <span class="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
+              <span className="block sm:inline">{message}</span>
+              <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
             </div>
           )}
         </div>
